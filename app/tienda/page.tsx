5f6c85406c924251521517cd0dfa922333e608b8
jorge <jorge504tapia@gmail.com>
2023-08-09 13:32:48 -0400

    actualization general

--- app/tienda/page.tsx
+++ app/tienda/page.tsx
@@ -1,4 +1,5 @@
 import PageShop from "@/components/shop/PageShop"
+import { IDesign } from "@/interfaces"
 
 async function fetchCategories () {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories`, {
@@ -16,6 +17,18 @@ async function fetchProducts () {
     }
   })
   return res.json()
+}
+
+export async function generateMetadata() {
+  const design: IDesign = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/design`, {
+    next: {
+      revalidate: 43200
+    }
+  }).then(res => res.json())
+
+  return {
+    title: design.home.seo.metaTitle
+  }
 }
 
 export default async function Page () {
