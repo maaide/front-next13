4702860ac0e903a7079be92a06cbf41f41265dc2
jorge <jorge504tapia@gmail.com>
2023-08-09 00:02:59 -0400

    actualization general

--- app/tienda/page.tsx
+++ app/tienda/page.tsx
@@ -1,6 +1,4 @@
 import PageShop from "@/components/shop/PageShop"
-import { IDesign } from "@/interfaces"
-import { Metadata } from "next"
 
 async function fetchCategories () {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories`, {
@@ -18,24 +16,6 @@ async function fetchProducts () {
     }
   })
   return res.json()
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  const design: IDesign = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/design`, {
-    next: {
-      revalidate: 43200
-    }
-  }).then((res) => res.json())
-
-  return {
-    title: design.shop.metaTitle,
-    description: design.shop.metaDescription,
-    openGraph: {
-      title: design.shop.metaTitle,
-      description: design.shop.metaDescription,
-      images: [design.shop.banner?.url !== '' ? design.shop.banner!.url : ''],
-    },
-  }
 }
 
 export default async function Page () {
