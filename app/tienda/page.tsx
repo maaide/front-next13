634f3c654b0b4d27a247b361267039063084ed0b
jorge <jorge504tapia@gmail.com>
2023-08-10 19:15:49 -0400

    actualization general

--- app/tienda/page.tsx
+++ app/tienda/page.tsx
@@ -2,30 +2,20 @@ import PageShop from "@/components/shop/PageShop"
 import { IDesign } from "@/interfaces"
 import { Metadata } from "next"
 
+export const revalidate = 60
+
 async function fetchCategories () {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories`, {
-    next: {
-      revalidate: 60
-    }
-  })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories`)
   return res.json()
 }
 
 async function fetchProducts () {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
-    next: {
-      revalidate: 60
-    }
-  })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
   return res.json()
 }
 
 export async function generateMetadata(): Promise<Metadata> {
-  const design: IDesign = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/design`, {
-    next: {
-      revalidate: 43200
-    }
-  }).then(res => res.json())
+  const design: IDesign = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/design`).then(res => res.json())
 
   return {
     title: design.shop.metaTitle,
