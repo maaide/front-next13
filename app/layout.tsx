730387288a952074a745f588ccf140a579f8f877
jorge <jorge504tapia@gmail.com>
2023-08-23 19:50:47 -0400

    actualization genrela

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,24 @@ export default function RootLayout({
                           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                         `}
                       </Script>
+                      <Script
+                        id="fb-pixel"
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', 195423146705415);
+                        fbq('track', 'PageView');
+                        `,
+                        }}
+                      />
                       <style jsx global>{`
                         p, span, button, a, input, textarea, select {
                           font-family: ${poppins.style.fontFamily};
