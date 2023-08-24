ddb316ca9deae52752fb36970032b1fed4991d7f
jorge <jorge504tapia@gmail.com>
2023-08-24 11:16:22 -0400

    actualization general

--- components/products/PageProduct.tsx
+++ components/products/PageProduct.tsx
@@ -88,7 +88,7 @@ export default function PageProduct ({ product }: { product: IProduct }) {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
         __html: `
-        fbq('track', 'ViewContent', {content_name: ${product.name}, content_category: ${product.category}, currency: "CLP", value: ${product.price}});
+        fbq('track', 'ViewContent', {content_name: "${product.name}", content_category: "${product.category.category}", currency: "CLP", value: ${product.price}});
         `,
         }}
       />
