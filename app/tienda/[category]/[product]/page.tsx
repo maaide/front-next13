73c81a09601706a4c6899b13a572768fb40fcf86
jorge <jorge504tapia@gmail.com>
2023-08-24 11:07:58 -0400

    actualization general

--- app/tienda/[category]/[product]/page.tsx
+++ app/tienda/[category]/[product]/page.tsx
@@ -2,7 +2,6 @@ import PageProduct from "@/components/products/PageProduct"
 import { IProduct } from "@/interfaces"
 import Cookies from 'js-cookie'
 import type { Metadata } from 'next'
-import Script from "next/script"
 
 export const revalidate = 60
 
@@ -43,35 +42,6 @@ export default async function ({ params }: { params: { product: string } }) {
   const product: IProduct = await fetchProduct(params.product)
 
   return (
-    <>
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-        __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID});
-        fbq('track', 'ViewContent', {content_name: ${product.name}, content_category: ${product.category}, currency: "CLP", value: ${product.price}});
-        `,
-        }}
-      />
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-        __html: `
-        fbq('track', 'ViewContent', { content_name: ${product.name}, content_category: ${product.category.category}, currency: "CLP", value: ${product.price} });
-        `,
-        }}
-      />
-      <PageProduct product={product} />
-    </>
+    <PageProduct product={product} />
   )
 }
--- components/products/PageProduct.tsx
+++ components/products/PageProduct.tsx
@@ -8,6 +8,7 @@ import { ButtonAddToCart, ButtonNone, ItemCounter, ProductSlider, Spinner } from
 import Link from "next/link"
 import Image from 'next/image'
 import { NumberFormat } from "@/utils"
+import Script from "next/script"
 
 export default function PageProduct ({ product }: { product: IProduct }) {
 
@@ -82,6 +83,24 @@ export default function PageProduct ({ product }: { product: IProduct }) {
 
   return (
     <>
+      <Script
+        id={`fb-pixel-${product.name}`}
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+        __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID});
+        fbq('track', 'ViewContent', {content_name: ${product.name}, content_category: ${product.category}, currency: "CLP", value: ${product.price}});
+        `,
+        }}
+      />
       {
         product?.stock > 0
           ? (
