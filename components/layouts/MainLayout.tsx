66cb3021b82c4ba5ffe8e8253697396764ad82cf
jorge <jorge504tapia@gmail.com>
2023-09-28 13:59:35 -0300

    actualization genreal

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -12,6 +12,7 @@ export const MainLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <Navbar menu={menu} setMenu={setMenu} setIndex={setIndex} index={index}>
+        <div className='h-[53px]' />
         { children }
         {
           pathname !== '/finalizar-compra'
